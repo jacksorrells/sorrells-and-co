666ce912974adaa75c7143f78e7d95c867292e93
Jack Sorrells <jack@jacksorrells.com>
2019-12-03 18:36:03 -0600

    test again

--- src/templates/community-page.js
+++ src/templates/community-page.js
@@ -51,7 +51,7 @@ export default class Page extends React.Component {
               {htmlToReact(_.get(this.props, "pageContext.html"))}
 
               {show_images && (
-                  <p>Let me just see if this guy works.</p>
+                  <Carousel views={images} />
               )}
             </div>
 
