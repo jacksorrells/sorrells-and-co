1d2691da9fc61bc2140f228e629ee8e5752f782b
Jack Sorrells <jack@jacksorrells.com>
2019-12-04 13:47:04 -0600

    cleaning up some more stuff

--- src/templates/community-page.js
+++ src/templates/community-page.js
@@ -22,23 +22,13 @@ export default class Page extends React.Component {
 
 
   render() {
-    let post_list = _.orderBy(_.filter(getPages(this.props.pageContext.pages, '/posts'), ['frontmatter.show_in_sidebar', true]), 'frontmatter.date', 'desc');
-    let post_len = _.size(post_list);
     console.log('community-page')
     console.log('this.props -> ', this.props);
     console.log('this.state -> ', this.state);
-    
     return (
       <Layout {...this.props}>
-        <section
-          id="main"
-          className={
-            "wrapper" +
-            (_.get(this.props, "pageContext.frontmatter.sidebar.enabled")
-              ? " sidebar " +
-                _.get(this.props, "pageContext.frontmatter.sidebar.side")
-              : "")
-          }
+        <section id="main"
+          className={"wrapper" + (_.get(this.props, "pageContext.frontmatter.sidebar.enabled") ? " sidebar " + _.get(this.props, "pageContext.frontmatter.sidebar.side") : "")}
         >
           <div className="inner">
             <header className="major">
@@ -59,36 +49,6 @@ export default class Page extends React.Component {
                 <Carousel views={this.state.images} />
               )}
             </div>
-
-            {_.get(this.props, "pageContext.frontmatter.sidebar.enabled") && (
-              <div className="sidebar">
-                {_.map(_.orderBy(_.filter(getPages(this.props.pageContext.pages, "/posts"), ["frontmatter.show_in_sidebar", true]), "frontmatter.date", "desc"),
-                  (post, post_idx) => (
-                    <React.Fragment key={post_idx}>
-                      <section key={post_idx}>
-                        {_.get(post, "frontmatter.alt_img") && (
-                          <Link to={safePrefix(_.get(post, "url"))} className="image fit">
-                            <img src={safePrefix(_.get(post, "frontmatter.alt_img"))} alt="" />
-                          </Link>
-                        )}
-                        <h3>{_.get(post, "frontmatter.title")}</h3>
-                        {markdownify(_.get(post, "frontmatter.excerpt"))}
-                        <footer>
-                          <ul className="actions">
-                            <li>
-                              <Link to={safePrefix(_.get(post, "url"))} className="button">
-                                Learn More
-                              </Link>
-                            </li>
-                          </ul>
-                        </footer>
-                      </section>
-                      {!(post_idx === post_len - 1) && <hr key={post_idx} />}
-                    </React.Fragment>
-                  )
-                )}
-              </div>
-            )}
           </div>
         </section>
       </Layout>
