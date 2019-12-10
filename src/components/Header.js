f7202a74b337583bb4ff34b4c258ed44fcc40ee6
Jack Sorrells <jack@jacksorrells.com>
2019-12-10 16:19:54 -0600

    Let me test this image thing now

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ export default class Header extends React.Component {
       <header id="header" className={classNames({'alt': _.get(this.props, 'pageContext.frontmatter.template') === _.get(this.props, 'pageContext.site.siteMetadata.landing_template')})}>
         <h1 id="logo">
           <Link to={_.get(this.props, 'pageContext.site.pathPrefix') || '/'}>
-            <img src={_.get(this.props, 'pageContext.site.siteMetadata.logo')} alt={_.get(this.props, 'pageContext.site.siteMetadata.title')} />
+            <img src={_.get(this.props, 'pageContext.site.siteMetadata.cloudinaryUrl') + "/" + _.get(this.props, 'pageContext.site.siteMetadata.logo')} alt={_.get(this.props, 'pageContext.site.siteMetadata.title')} />
           </Link>
         </h1>
         <nav id="nav">
