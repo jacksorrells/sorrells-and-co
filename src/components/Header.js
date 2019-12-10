ea967b3d7685d52ff0febb0944f67de90de1be2a
Jack Sorrells <jack@jacksorrells.com>
2019-12-10 15:29:08 -0600

    further test

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,16 @@ export default class Header extends React.Component {
   render() {
 
     console.log('this.props -> ', this.props);
+    console.log('this.props.pageContext.site.siteMetadata -> ', this.props.pageContext.site.siteMetadata);
 
     let menu = _.get(this.props, 'pageContext.menus.main');
     return (
       <header id="header" className={classNames({'alt': _.get(this.props, 'pageContext.frontmatter.template') === _.get(this.props, 'pageContext.site.siteMetadata.landing_template')})}>
-        <h1 id="logo"><Link to={_.get(this.props, 'pageContext.site.pathPrefix') || '/'}>{_.get(this.props, 'pageContext.site.siteMetadata.title')}</Link></h1>
+        <h1 id="logo">
+          <Link to={_.get(this.props, 'pageContext.site.pathPrefix') || '/'}>
+            {_.get(this.props, 'pageContext.site.siteMetadata.title')}
+          </Link>
+        </h1>
         <nav id="nav">
           <Menu {...this.props} menu={menu} page={this.props.pageContext} />
         </nav>
