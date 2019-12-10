1fa0982cc1f50457b19b669585445e60104b0609
Jack Sorrells <jack@jacksorrells.com>
2019-12-10 14:34:56 -0600

    Let us see what happens now

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import _ from 'lodash';
 
 import { Layout } from '../components/index';
@@ -9,6 +9,8 @@ const encode = (data) => {
     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
     .join("&");
 }
+
+const [name, setName] = useState("");
 
 export default class Contact extends React.Component {
   constructor(props){
@@ -59,12 +61,10 @@ export default class Contact extends React.Component {
               {htmlToReact(_.get(this.props, "pageContext.html"))}
 
               <form name="contact" method="POST" data-netlify="true" netlify netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
-                <div className="col-12">
+                <input type="hidden" name="form-name" value="contact" />
+
                   <input type="text" name="name" id="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} />
-                </div>
-                <div className="col-12">
                   <input type="email" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
-                </div>
                 <div className="col-12">
                   <input type="text" name="phone" id="phone" placeholder="Phone" value={this.state.phone} onChange={this.handleChange} />
                 </div>
