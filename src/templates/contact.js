75c57819bc61b057cd7e2f274e221ac5f080a77b
Jack Sorrells <jack@jacksorrells.com>
2019-12-10 14:28:49 -0600

    okay, now let's test this guy otu

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import _ from 'lodash';
 
 import { Layout } from '../components/index';
 import { markdownify, Link, toUrl, safePrefix, htmlToReact } from '../utils';
+import { encode } from 'punycode';
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
 export default class Contact extends React.Component {
-  const [name, setName] = useState('');
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
+    };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
 
   render() {
 
@@ -27,18 +59,18 @@ export default class Contact extends React.Component {
               )}
               {htmlToReact(_.get(this.props, "pageContext.html"))}
 
-              <form name="contact" method="POST" data-netlify="true" netlify netlify-honeypot="bot-field">
+              <form name="contact" method="POST" data-netlify="true" netlify netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
                 <div className="col-12">
-                  <input type="text" name="name" id="name" placeholder="Name" />
+                  <input type="text" name="name" id="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} />
                 </div>
                 <div className="col-12">
-                  <input type="email" name="email" id="email" placeholder="Email" />
+                  <input type="email" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
                 </div>
                 <div className="col-12">
-                  <input type="text" name="phone" id="phone" placeholder="Phone" />
+                  <input type="text" name="phone" id="phone" placeholder="Phone" value={this.state.phone} onChange={this.handleChange} />
                 </div>
                 <div className="col-12">
-                  <textarea name="message" id="message" placeholder="Message" rows="4" />
+                  <textarea name="message" id="message" placeholder="Message" rows="4" value={this.state.message} onChange={this.handleChange} />
                 </div>
                 <div className="col-12">
                   <button type="submit" className="primary button fit">Send Message</button>
