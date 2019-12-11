7560c43b79734c4259bd5b25b8410f3ced2c2df9
Jack Sorrells <jack@jacksorrells.com>
2019-12-11 11:59:32 -0600

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,19 +11,6 @@ export default class Footer extends React.Component {
             <footer id="footer">
                 <div className="content">
                     <div className="inner">
-                        {_.get(this.props, 'pageContext.site.data.footer.about.enabled') && 
-                            <section className="about">
-                                <h3>{_.get(this.props, 'pageContext.site.data.footer.about.title')}</h3>
-                                {markdownify(_.get(this.props, 'pageContext.site.data.footer.about.text'))}
-                                {_.get(this.props, 'pageContext.site.data.footer.about.actions') && 
-                                    <ul className="actions">
-                                        {_.map(_.get(this.props, 'pageContext.site.data.footer.about.actions'), (action, action_idx) => (
-                                            <li key={action_idx}><Link to={(_.get(action, 'url').startsWith('#') ? _.get(action, 'url') : safePrefix(_.get(action, 'url')))} className={classNames('button', {'primary': _.get(action, 'is_primary')}, {'scrolly': _.get(action, 'is_scrolly')})}>{_.get(action, 'label')}</Link></li>
-                                        ))}
-                                    </ul>
-                                }
-                            </section>
-                        }
                         {_.get(this.props, 'pageContext.site.data.footer.contact_methods.enabled') && <React.Fragment>
                             {_.get(this.props, 'pageContext.site.data.footer.contact_methods.methods_col1') && 
                                 <ul className="icons bulleted">
@@ -61,13 +48,13 @@ export default class Footer extends React.Component {
                             }
                         </React.Fragment>}
                     </div>
-                </div>
-                <p className="copyright">{_.get(this.props, 'pageContext.site.data.footer.copyright.enabled') && <React.Fragment>{htmlToReact(_.get(this.props, 'pageContext.site.data.footer.copyright.text'))}<br /></React.Fragment>}
-                    {
-                    _.map(_.get(this.props, 'pageContext.menus.footer'), (item, item_idx) => (<React.Fragment key={item_idx}>
+                    <p className="copyright">{_.get(this.props, 'pageContext.site.data.footer.copyright.enabled') && <React.Fragment>{htmlToReact(_.get(this.props, 'pageContext.site.data.footer.copyright.text'))}<br /></React.Fragment>}
+                      {_.map(_.get(this.props, 'pageContext.menus.footer'), (item, item_idx) => (<React.Fragment key={item_idx}>
                         <Link key={item_idx} to={(_.get(item, 'url').startsWith('#') ? _.get(item, 'url') : safePrefix(_.get(item, 'url')))}>{_.get(item, 'title')}</Link>{(!(item_idx === item_len - 1)) && ' | '}
-                    </React.Fragment>))}
-                </p>
+                      </React.Fragment>))}
+                    </p>
+                </div>
+                
             </footer>
         );
     }
